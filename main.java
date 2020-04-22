c34e644a4e7cf382738df9d52d89a2790167c2ee
ArielM24 <ariel_montoya15@outlook.es>
2020-04-22 09:41:48 -0500

    native look and feel

--- main.java
+++ main.java
@@ -1,5 +1,12 @@
+import javax.swing.UIManager;
+
 public class main {
 	public static void main(String[] args) {
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
 		window w = new window();
 		w.setBounds(0,0,700,350);
 		w.setLocationRelativeTo(null);
